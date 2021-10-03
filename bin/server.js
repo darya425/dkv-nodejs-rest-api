1078b0cbc4008acaef77560273160bd8a21cdc7f
darya425 <dasha425@mail.ru>
2021-10-03 12:18:05 +0200

    added hw-3

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
 const app = require("../app");
+require("dotenv").config();
 
-const { PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connect success");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
--- controllers/add.js
+++ controllers/add.js
@@ -1,7 +1,7 @@
-const { addContact } = require("../model/contacts");
+const { Contact } = require("../models");
 
-const add = async (req, res, next) => {
-  const result = await addContact(req.body);
+const add = async (req, res) => {
+  const result = await Contact.create(req.body);
   res.status(201).json({ status: "success", code: 201, data: { result } });
 };
 
